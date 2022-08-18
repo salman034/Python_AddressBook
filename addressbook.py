16d01cc733b63f81fe7da3ddd5d94e6168b01ac5
unknown <you@example.com>
2022-08-18 23:38:21 +0530

    Added: Ability to create new AddressBook

--- addressbook.py
+++ addressbook.py
@@ -1,6 +1,3 @@
-# Ability to create a Contacts in Address Book with first and last names, address,
-# city, state, zip, phone number and email...
-
 class Contact:
     def __init__(self, first_name, last_name, address, city, state, pin_code, email_id, mobile_number):
         self.first_name = first_name
@@ -12,9 +9,23 @@ class Contact:
         self.email_id = email_id
         self.mobile_number = mobile_number
 
-        print(self.first_name, self.last_name, self.address, self.city, self.state, self.pin_code, self.email_id, self.mobile_number)
+        details = [self.first_name, self.last_name, self.address, self.city, self.state, self.pin_code,
+                   self.email_id, self.mobile_number]
+
+        print(details)
 
 
 if __name__ == "__main__":
     print("Welcome to address book")
-    Contact('Salman|', 'Pasha|', 'BTM 1st stage|', 'Bangalore|', 'Karnataka |', '560029 |', 'mkspasha@gmail.com |', '9845715264')
+    user_input = int(input("Enter your choice\n1:Add the contact\n2:Edit the contact\n3:Delete the contact\n4:display"
+                           "\n5:exit: "))
+    if user_input == 1:
+        f_name = input("Enter first name: ")
+        l_name = input("Enter last name: ")
+        address = input("Enter the address: ")
+        city = input("Enter the city: ")
+        state = input("Enter the state: ")
+        pincode = input("Enter the pin code: ")
+        email = input("Enter email id: ")
+        mobile_number = input("Enter mobile number: ")
+        Contact(f_name, l_name, address, city, state, pincode, email, mobile_number)
