36fdc5ed837451a2627f01c05f8fc9d92229cba8
unknown <you@example.com>
2022-08-19 00:54:41 +0530

    Added: Ability to Edit the contact

--- addressbook.py
+++ addressbook.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Contact:
     def __init__(self, first_name, last_name, address, city, state, pin_code, email_id, mobile_number):
         self.first_name = first_name
@@ -8,24 +11,45 @@ class Contact:
         self.pin_code = pin_code
         self.email_id = email_id
         self.mobile_number = mobile_number
-
-        details = [self.first_name, self.last_name, self.address, self.city, self.state, self.pin_code,
-                   self.email_id, self.mobile_number]
-
-        print(details)
+        self.details = [self.first_name, self.last_name, self.address, self.city, self.state, self.pin_code,
+                        self.email_id, self.mobile_number]
 
 
 if __name__ == "__main__":
     print("Welcome to address book")
-    user_input = int(input("Enter your choice\n1:Add the contact\n2:Edit the contact\n3:Delete the contact\n4:display"
-                           "\n5:exit: "))
-    if user_input == 1:
-        f_name = input("Enter first name: ")
-        l_name = input("Enter last name: ")
-        address = input("Enter the address: ")
-        city = input("Enter the city: ")
-        state = input("Enter the state: ")
-        pincode = input("Enter the pin code: ")
-        email = input("Enter email id: ")
-        mobile_number = input("Enter mobile number: ")
-        Contact(f_name, l_name, address, city, state, pincode, email, mobile_number)
+    contact_list = []
+    while True:
+        user_input = int(
+            input("Enter your choice\n1:Add the contact\n2:Edit the contact\n3:Delete the contact\n4:Display"
+                  "\n5:Exit: "))
+        if user_input == 1:
+            f_name = input("Enter first name: ")
+            l_name = input("Enter last name: ")
+            address = input("Enter the address: ")
+            city = input("Enter the city: ")
+            state = input("Enter the state: ")
+            pincode = input("Enter the pin code: ")
+            email = input("Enter email id: ")
+            mobile_number = input("Enter mobile number: ")
+            result = Contact(f_name, l_name, address, city, state, pincode, email, mobile_number)
+            contact_list.append(result.details)
+            print(contact_list)
+        elif user_input == 2:
+            f_name = input("Enter first name: ")
+            l_name = input("Enter last name: ")
+            for i in contact_list:
+                if f_name == i[0] and l_name == i[1]:
+                    print("Contact found, please enter details to update")
+                    f_name = input("Enter first name: ")
+                    l_name = input("Enter last name: ")
+                    address = input("Enter the address: ")
+                    city = input("Enter the city: ")
+                    state = input("Enter the state: ")
+                    pincode = input("Enter the pin code: ")
+                    email = input("Enter email id: ")
+                    mobile_number = input("Enter mobile number: ")
+                    index = contact_list.index(i)
+                    contact_list[index] = [f_name, l_name, address, city, state, pincode, email, mobile_number]
+            print(contact_list)
+        elif user_input == 5:
+            sys.exit()
